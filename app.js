397b419c0f6eb4c571cf567820c084be1404d138
Hussien-Talal-Majeed <hossientalal2@gmail.com>
2020-06-04 00:33:18 +0300

    changing the api and doing the request

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const API_URL = "http://api.currencylayer.com/";
-const ACCESS_KEY ="7421bc79cc9ffa900eca4a37c8d104bd";
+const API_URL = "https://free.currconv.com/api/v7";
+const ACCESS_KEY ="b12a6953b1207140aa3f";
 document.addEventListener('DOMContentLoaded', ()=>{
     fetch(`https://free.currconv.com/api/v7/currencies?apiKey=b12a6953b1207140aa3f`)
     .then(response => response.json())
@@ -13,9 +13,32 @@ function addCurrencyList(currenciesObj){
     }
 }
 const fromSelectList = document.getElementById('from');
+const amount = document.getElementById('amount');
 const toSelectList = document.getElementById('to');
-console.log(fromSelectList);
 function addCurrenciesToSelect(key, value){
     toSelectList.insertAdjacentHTML("beforeend", `<option value=${key}>${value}</option>`);
     fromSelectList.insertAdjacentHTML("beforeend", `<option value=${key}>${value}</option>`);
+}
+
+const convertBtn = document.getElementById('convertBtn');
+convertBtn.addEventListener('click', (e)=> {
+    e.preventDefault();
+    const fromValue = fromSelectList.value;
+    const toValue = toSelectList.value;
+    const amountValue = amount.value;
+    getResult(fromValue, toValue, amountValue);
+})
+
+function getResult(from, to, amount){
+    const query = from + '_'+to;
+    const configObj = {
+        method : "POST",
+        headers : {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    }
+    fetch(API_URL+"/convert?q="+query+"&compact=ultra&apiKey="+ACCESS_KEY)
+    .then(response => response.json())
+    .then(data => console.log(data));
 }
