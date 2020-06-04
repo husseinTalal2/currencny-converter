f6b4a71897aad94a248c2fa7a01e91eafe7d55a2
Hussien-Talal-Majeed <hossientalal2@gmail.com>
2020-06-04 13:45:16 +0300

    formating the result

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ function showResult(resultObj, amountValue, query){
     const currencies = query.split("_");
     const from = currencies[0];
     const to = currencies[1];
-    result.innerText = `${amountValue} ${from} = ${resultObj[query]*amountValue} ${to}`;
+    const formatedResult = parseFloat((resultObj[query]*amountValue).toFixed(2)).toLocaleString();
+    result.innerText = `${amountValue} ${from} = ${formatedResult} ${to}`;
 }
 document.addEventListener('DOMContentLoaded', ()=>{
     fetch(`https://free.currconv.com/api/v7/currencies?apiKey=b12a6953b1207140aa3f`)
